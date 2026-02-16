bda9ee389668d10f073ec4a5baf0595aa76a629f
teo <teo@teoPC>
2026-02-16 17:52:22 +0100

    Aggiunta classe Carrello

---
+++ codice/NeedForSpecs/src/it/unipv/pois/IlParadisoDellaJVM/NeedForSpecs/model/account/Carrello.java
@@ -0,0 +1,21 @@
+package it.unipv.pois.IlParadisoDellaJVM.NeedForSpecs.model.account;
+
+import java.util.ArrayList;
+
+public class Carrello {
+	
+	private ArrayList<Annuncio> prodotti;
+	private double prezzo_totale;
+	
+	
+	
+	public Carrello(ArrayList<Annuncio> prodotti, double prezzo_totale) {
+		super();
+		this.prodotti = prodotti;
+		this.prezzo_totale = prezzo_totale;
+	}
+	
+	
+		
+
+}
